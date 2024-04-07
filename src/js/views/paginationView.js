f722b20575e78e7730cbe2bf4ddfd12f7411c0e1
lei <littlesomerest@gmail.com>
2024-04-08 07:57:39 +0800

    refactor _generateMarkup function

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -9,6 +9,7 @@ class PaginationView extends View {
       const btn = e.target.closest('.btn--inline');
       if (!btn) return;
 
+      // `+` convert string to number
       const goToPage = +btn.dataset.goto;
       subscriber(goToPage);
     });
@@ -20,51 +21,43 @@ class PaginationView extends View {
       this._data.results.length / this._data.resultPerPage
     );
     // Page 1, there are other page
-    if (curPage === 1 && numPages > 1)
-      return `
-        <button data-goto="${
-          curPage + 1
-        }" class="btn--inline pagination__btn--next">
-          <span>Page ${curPage + 1}</span>
-          <svg class="search__icon">
-            <use href="${icons}#icon-arrow-right"></use>
-          </svg>
-        </button>
-       `;
+    if (curPage === 1 && numPages > 1) return this._generateMarkupNext(curPage);
     // Last Page
     if (curPage === numPages && numPages > 1)
-      return `
-        <button data-goto="${
-          curPage - 1
-        }" class="btn--inline pagination__btn--prev">
-          <svg class="search__icon">
-            <use href="${icons}#icon-arrow-left"></use>
-          </svg>
-          <span>Page ${curPage - 1}</span>
-        </button>
-    `;
+      return this._generateMarkupPrev(curPage);
     // Middle page
     if (curPage < numPages)
-      return `
-        <button data-goto="${
-          curPage + 1
-        }" class="btn--inline pagination__btn--next">
-          <span>Page ${curPage + 1}</span>
-          <svg class="search__icon">
-            <use href="${icons}#icon-arrow-right"></use>
-          </svg>
-        </button>
-        <button data-goto="${
-          curPage - 1
-        }" class="btn--inline pagination__btn--prev">
-          <svg class="search__icon">
-            <use href="${icons}#icon-arrow-left"></use>
-          </svg>
-          <span>Page ${curPage - 1}</span>
-        </button>
-   `;
+      return `${this._generateMarkupPrev(curPage)} ${this._generateMarkupNext(
+        curPage
+      )}`;
     // Page 1, there are NO other page
     return '';
+  }
+
+  _generateMarkupNext(curPage) {
+    return `
+    <button data-goto="${
+      curPage + 1
+    }" class="btn--inline pagination__btn--next">
+      <span>Page ${curPage + 1}</span>
+      <svg class="search__icon">
+        <use href="${icons}#icon-arrow-right"></use>
+      </svg>
+    </button>
+    `;
+  }
+
+  _generateMarkupPrev(curPage) {
+    return `
+    <button data-goto="${
+      curPage - 1
+    }" class="btn--inline pagination__btn--prev">
+      <svg class="search__icon">
+        <use href="${icons}#icon-arrow-left"></use>
+      </svg>
+      <span>Page ${curPage - 1}</span>
+    </button>
+    `;
   }
 }
 
