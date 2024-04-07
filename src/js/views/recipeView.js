835a9ebcb3ae569571b28a7f89eb9ec54b3d2c02
lei <littlesomerest@gmail.com>
2024-04-07 10:08:36 +0800

    add fractional dependency

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,6 @@
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../../img/icons.svg'; // Parcel 2
+import { Fraction } from 'fractional';
 
 class RecipeView {
   #parentEl = document.querySelector('.recipe');
@@ -94,7 +95,9 @@ class RecipeView {
                 <svg class="recipe__icon">
                   <use href="${icons}.svg#icon-check"></use>
                 </svg>
-                <div class="recipe__quantity">${ing.quantity}</div>
+                <div class="recipe__quantity">${
+                  ing.quantity ? new Fraction(ing.quantity).toString() : ''
+                }</div>
                 <div class="recipe__description">
                   <span class="recipe__unit">${ing.unit}</span>
                   ${ing.description}
