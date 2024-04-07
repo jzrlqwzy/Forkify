516c6a112657d33fd00b0eaa0a882c44019ba2ae
lei <littlesomerest@gmail.com>
2024-04-07 09:00:29 +0800

    add event listener when loading recipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -30,11 +30,16 @@ const renderspinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+
+    // Guard clause
+    if (!id) return;
+
     // 1 Loading recipe
     renderspinner(recipeContainer);
 
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -157,4 +162,6 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
