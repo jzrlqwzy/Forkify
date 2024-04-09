0542941420061e385aaaeb9dc41bb49b987a1610
lei <littlesomerest@gmail.com>
2024-04-09 08:56:52 +0800

    load&search recipe with user key

--- src/js/model.js
+++ src/js/model.js
@@ -31,7 +31,7 @@ const createRecipeObject = function (data) {
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await AJAX(`${API_URL}${id}`);
+    const data = await AJAX(`${API_URL}${id}?key=${API_KEY}`);
     state.recipe = createRecipeObject(data);
 
     // add `bookmarked` mark to every loaded recipe at beginning
@@ -50,7 +50,7 @@ export const loadRecipe = async function (id) {
 export const loadSearchResult = async function (query) {
   try {
     state.search.query = query;
-    const data = await AJAX(`${API_URL}?search=${query}`);
+    const data = await AJAX(`${API_URL}?search=${query}&key=${API_KEY}`);
     console.log(data);
 
     state.search.results = data.data.recipes.map(recipe => {
