59abd017300b6852ad99575a35f4796fb7327792
lei <littlesomerest@gmail.com>
2024-04-09 10:22:21 +0800

    change dependency from fractional to fracty

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,7 @@
 import View from './View.js';
 import icons from 'url:../../img/icons.svg'; // Parcel 2
 import { Fraction } from 'fractional';
+import fracty from 'fracty';
 
 class RecipeView extends View {
   _parentEl = document.querySelector('.recipe');
@@ -130,7 +131,7 @@ class RecipeView extends View {
         <use href="${icons}#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
-        ing.quantity ? new Fraction(ing.quantity).toString() : ''
+        ing.quantity ? fracty(ing.quantity) : ''
       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
